13b0ed63585dee6c6300cd28b7ac7c6d2cb3ac6d
nawalhussain <44042013+nawalhussain@users.noreply.github.com>
2021-09-21 09:45:34 -0400

    Add files via upload

---
+++ naw_rec_data.js
@@ -0,0 +1,15 @@
+
+const restaurants = [
+  { "name":"Cluck Clucks", "city":"toronto", "website": "https://www.cluckclucks.ca/", "image": "images/cluckclucks1.jpg",
+   "rating": 0.9, "cuisine": "american", "review": "xyz", "recommended": "If you want to try amazing chicken and waffles visit their website at " },
+  { "name":"Kunafas", "city":"toronto", "website": "http://kunafas.com/", "image": "images/kunafas.png",
+  "rating": 0.8, "cuisine": "arab", "review": "xyz", "recommended": "Sweet soft and crucnchy Kunafas. An absolute must try " },
+  { "name":"Lazeez", "city":"waterloo", "website": "https://www.lazeezshawarma.com/", "image": "images/lazeez.png",
+  "rating": 0.7, "cuisine": "arab", "review": "xyz", "recommended": "No frills counter serve shawarma open late at night " },
+  { "name":"Empress of India", "city":"waterloo", "website": "https://www.empressofindia.ca/", "image": "images/indian.png",
+  "rating": 0.8, "cuisine": "indian", "review": "xyz", "recommended": "Some of the best Indian food in Waterloo at " },
+  { "name":"Aunty's Kitchen", "city":"waterloo", "website": "https://order.incentivio.com/c/auntyskitchen/", "image": "images/auntys.png",
+  "rating": 0.6, "cuisine": "indian", "review": "xyz", "recommended": "Nothin is mind blowing but good paratha rolls " },
+  { "name":"Dessert Lady Cafe", "city":"toronto", "website": "https://www.dessertlady.ca/", "image": "images/dessert_lady.png",
+  "rating": 0.5, "cuisine": "american", "review": "xyz", "recommended": "Cute seating area. Some cakes are good but place is overpriced " }
+]
